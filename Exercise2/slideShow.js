87e41ab6671fa2fec43eef4c61c667b37e4b916d
Andorryu <adority121@gmail.com>
2022-11-02 14:48:09 -0500

    wrote the start of slideshow.js and gave ids to buttons

--- Exercise2/slideShow.js
+++ Exercise2/slideShow.js
@@ -1,1 +1,9 @@
 
+window.addEventListener("load", () => {
+    // find img and buttons
+    let img = document.querySelector("img");
+    let prevButton = document.getElementById("prev");
+    let nextButton = document.getElementById("next");
+
+    
+});
