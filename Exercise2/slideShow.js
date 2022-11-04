63cac9b6ea2d422d1819931ceac0351c72622328
Andorryu <adority121@gmail.com>
2022-11-03 20:52:39 -0500

    finished exercise2

--- Exercise2/slideShow.js
+++ Exercise2/slideShow.js
@@ -4,6 +4,19 @@ window.addEventListener("load", () => {
     let img = document.querySelector("img");
     let prevButton = document.getElementById("prev");
     let nextButton = document.getElementById("next");
-
+    let images = ["1.jpg", "2.jpg", "3.jpg", "4.PNG", "5.jpg"];
+    // initialize first image
+    let i = 0;
+    img.src="img/"+images[i];
     
+    prevButton.addEventListener("click", () => {
+        i--;
+        if (i < 0) i = 4;
+        img.src="img/"+images[i];
+    })
+    nextButton.addEventListener("click", () => {
+        i++;
+        if (i > 4) i = 0;
+        img.src="img/"+images[i];
+    })
 });
