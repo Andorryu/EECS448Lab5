2f702096a3f4d5fdc788082466329c91457ca1d3
AndrewDority <adority121@gmail.com>
2022-11-09 23:30:10 -0600

    refactored

--- nav.js
+++ nav.js
@@ -2,25 +2,29 @@
 // unmodifiable
 const navItems = [
     {
-        title: "Home",
+        title: "Lab 5 Home",
+        href: "index.html"
+    },
+    {
+        title: "Lab 6 Home",
         href: "index.html"
     },
     {
         title: "Exercise 1",
-        href: "password_validator.html"
+        href: "1.html"
     },
     {
         title: "Exercise 2",
-        href: "slide_show.html"
+        href: "2.html"
     },
     {
         title: "Exercise 3",
-        href: "profile.html"
+        href: "3.html"
     },
     {
         title: "Exercise 4",
-        href: "css_manipulation.html"
-    }
+        href: "4.html"
+    },
 ];
 
 // modifiable
@@ -28,16 +32,19 @@ const navItems = [
 // these are the defaults for any html file within an "Exercise#" folder
 // since most html files are in those folders
 let hrefPrefixes = {
-    "Home": "../",
+    "Lab 5 Home": "../",
+    "Lab 6 Home": "../",
     "Exercise 1": "../Exercise1/",
     "Exercise 2": "../Exercise2/",
     "Exercise 3": "../Exercise3/",
     "Exercise 4": "../Exercise4/",
 };
+let lab6Mode = false;
 
 window.addEventListener("load", () => {
     let nav = document.querySelector("nav");
     for (let item of navItems) {
+        if (lab6Mode && item.title == "Exercise 4") continue; // skip exercise 4 if in lab6
         let a = nav.appendChild(document.createElement("a"));
         //let div = a.appendChild(document.createElement("div"));
         //div.textContent = item.title;
