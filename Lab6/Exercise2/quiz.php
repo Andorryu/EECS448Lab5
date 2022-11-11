4813eea29c9d6fbf9ea878dd75b84f7f26e18e75
Andorryu <adority121@gmail.com>
2022-11-11 16:36:46 -0600

    refactored question statements

--- Lab6/Exercise2/quiz.php
+++ Lab6/Exercise2/quiz.php
@@ -1,42 +1,47 @@
 <?php
 
-// get answer values for each question
 $qs = array();
+$qv = array();
+
+// question statements
+$qs[0] = "Which color is the best?";
+$qs[1] = "How many fingers am I holding up?";
+$qs[2] = "Who is your favorite student?";
+$qs[3] = `<p>
+            <math display="inline">
+                <mrow>
+                    <munderover>
+                        <mo>∑</mo>
+                        <mrow>
+                            <mi>n</mi>
+                            <mo>=</mo>
+                            <mn>1</mn>
+                        </mrow>
+                        <mrow>
+                            <mo>+</mo>
+                            <mn>∞</mn>
+                        </mrow>
+                    </munderover>
+                    <mfrac>
+                        <mn>1</mn>
+                        <msup>
+                            <mi>n</mi>
+                         <mn>2</mn>
+                        </msup>
+                    </mfrac>
+                </mrow>
+            </math>
+          </p>`;
+$qs[4] = "Did you enjoy this quiz?";
+
+// question values
 for ($i = 0; $i < 5; $i++) {
-    $qs[$i] = $_POST["q".($i + 1)];
+    $qv[$i] = $_POST["q".($i + 1)];
 }
 
+// write questions back
 for ($i = 0; $i < 5; $i++) {
     $j = $i+1;
-    echo "Question $j: " . (
-    $j==1 ? "Which color is the best?" :
-    $j==2 ? "How many fingers am I holding up?" :
-    $j==3 ? "Who is your favorite student?" :
-    $j==4 ? `<p>
-                <math display="inline">
-                    <mrow>
-                        <munderover>
-                            <mo>∑</mo>
-                            <mrow>
-                                <mi>n</mi>
-                                <mo>=</mo>
-                                <mn>1</mn>
-                            </mrow>
-                            <mrow>
-                                <mo>+</mo>
-                                <mn>∞</mn>
-                            </mrow>
-                        </munderover>
-                        <mfrac>
-                            <mn>1</mn>
-                            <msup>
-                                <mi>n</mi>
-                                <mn>2</mn>
-                            </msup>
-                        </mfrac>
-                    </mrow>
-                </math>
-            </p>` :
-    "Did you enjoy this quiz?");
+    echo "Question $j: " . $qs[$i];
 }
 ?>
