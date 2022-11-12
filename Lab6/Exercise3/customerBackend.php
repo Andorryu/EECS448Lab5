4f85ef0b04b5ef8d39cd009c6d356063fc566ba2
AndrewDority <adority121@gmail.com>
2022-11-12 17:27:17 -0600

    basic php structure

--- Lab6/Exercise3/customerBackend.php
+++ Lab6/Exercise3/customerBackend.php
@@ -1,3 +1,31 @@
 <?php
-echo "hey"
+
+// echo start of file
+echo '
+<!DOCTYPE html>
+<html>
+    <head>
+        <script src="../../nav.js"></script>
+        <link rel="stylesheet" href="../../style.css">
+        <script>
+            hrefPrefixes["Lab 5 Home"] = "../../Lab5/";
+            navItems[3].href = "Quiz.html";
+            navItems[4].href = "customerFrontend.html";
+            lab6Mode = true;
+        </script>
+        <script src="formChecker.js"></script>
+        <link rel="stylesheet" href="style.css">
+    </head>
+    <body>
+        <nav></nav>
+        <h1>Here\'s your receipt!</h1>
+        <br>
+';
+
+// echo end of file
+echo '
+    </body>
+</html>
+';
+
 ?>
