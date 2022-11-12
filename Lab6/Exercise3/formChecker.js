6ca0da7c9f4da8cd324248ba0579442bfcc28767
AndrewDority <adority121@gmail.com>
2022-11-12 00:29:36 -0600

    basic formChecker and refactored items in html

---
+++ Lab6/Exercise3/formChecker.js
@@ -0,0 +1,15 @@
+
+window.addEventListener("load", () => {
+    let form = document.querySelector("form");
+    
+    // when user submits form
+    form.addEventListener("submit", event => {
+        event.preventDefault();
+        /* 
+            Test if failure conditions are met.
+            Failure Conditions:
+            
+        */ 
+        if ()
+    });
+});
