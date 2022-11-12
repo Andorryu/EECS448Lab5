0638cb9e312739a4faef7cdd02665cc3d0ec7ceb
AndrewDority <adority121@gmail.com>
2022-11-12 01:38:51 -0600

    built formCheckerjs and style.css

--- Lab6/Exercise3/formChecker.js
+++ Lab6/Exercise3/formChecker.js
@@ -1,6 +1,12 @@
 
 window.addEventListener("load", () => {
     let form = document.querySelector("form");
+    let usernameField = document.getElementsByName("username")[0];
+    let passwordField = document.getElementsByName("password")[0];
+    let vacuumNum = document.getElementsByName("Vacuum")[0];
+    let carNum = document.getElementsByName("Car")[0];
+    let catNum = document.getElementsByName("Cat?")[0];
+    let shippingOptions = document.getElementsByName("shipping");
     
     // when user submits form
     form.addEventListener("submit", event => {
@@ -8,8 +14,35 @@ window.addEventListener("load", () => {
         /* 
             Test if failure conditions are met.
             Failure Conditions:
-            
-        */ 
-        if ()
+            - blank text fields
+            -- username
+            -- password
+            -- item quantities (cant be negative either)
+            -- at least one radio option should be selected
+            - username must be in the form of an email
+        */
+
+        // check empty things
+        if (usernameField.value == "" ||
+        passwordField.value == "" ||
+        vacuumNum.value == "" ||
+        carNum.value == "" ||
+        catNum.value == "") alert("empty string");
+
+        // check email format
+        // break loop if '.' is found after '@'
+        let email = false;
+        loop:
+        for (let i = 0; i < usernameField.value.length; i++) {
+            if (usernameField.value[i] == '@') {
+                for (let j = i; j < usernameField.value.length; j++) {
+                    if (usernameField.value[j] == '.') email = true;
+                }
+            }
+        }
+        if (!email) alert("username is not email");
+
+        // instead of checking if quantity values are negative, I just set a min of 0 in the html inputs themselves
+        // I also autochecked the first radio value so I dont have to check if theres a value for them
     });
 });
